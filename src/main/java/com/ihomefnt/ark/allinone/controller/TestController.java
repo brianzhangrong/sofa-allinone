e29161fdea1a8adc94a51930b4a8e0459dfd2232
zhangrong <39478871+brianzhangrong@users.noreply.github.com>
2021-01-21 08:55:56 +0800

    ok

--- src/main/java/com/ihomefnt/ark/allinone/controller/TestController.java
+++ src/main/java/com/ihomefnt/ark/allinone/controller/TestController.java
@@ -1,15 +1,22 @@
 package com.ihomefnt.ark.allinone.controller;
 
+import com.alipay.sofa.runtime.api.annotation.SofaReference;
+import com.ihomefnt.ark.inter.Demo1Interface;
+import com.ihomefnt.ark.inter.Demo2Interface;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @SofaReference
+    private Demo2Interface demo2Interface;
+    @SofaReference
+    private Demo1Interface demo1Interface;
     @GetMapping("test")
     public String test(){
 //        new com.ihomefnt.ark.demo.Demo1Service().init();
-        return "test";
+        return demo2Interface.init()+"--->"+demo1Interface.demo1();
     }
 
 }
